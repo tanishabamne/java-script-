76657cc7674e28016c97d14d000d29f0996fb9be
tanishabamne <tanisha.bamne1980@gmail.com>
2024-11-18 09:32:36 -0800

    first commit

---
+++ day1.js
@@ -0,0 +1,11 @@
+
+'use strict'         // it is use to show  every error 
+        var num=66;  // var is variable,   num is container which contains value
+        var num2="Tanisha";
+        console.log( "num value is" +  " "+ num  + "  " + "end")  
+        console.log(typeof(num))
+        console.log(num2)            //it is use to print value of num at console
+        console.log(typeof(num2))    //it is use to print type of num2 at console
+        var num1=6759829371543567890878n  // it is  'n'  use to show all valuse  
+        console.log(num1)
+        console.log(typeof(num1))
---
+++ day3.js
@@ -0,0 +1,40 @@
+ //function operator()
+// {
+//     alert("hello i m alert message") // only okk mesg
+// }
+// operator();
+
+
+// function operator1()
+// {
+//     prompt("hello i m prompt message") //  okk and mesg option
+// }
+// operator1();
+
+
+// function operator21()
+// {
+//     confirm("hello i m confirm message") // okk and cancel
+// }
+// operator2();
+
+// function ope()
+// {
+    
+
+//     let num1= parseInt( prompt( " enter any number" ) );   //parseInt  and number use for  int
+
+//     let num2= parseInt(prompt( " enter any number") );     // number is also use for number and float
+//     alert( num1+num2);
+
+
+// }
+// ope();
+let num=prompt("enter your age")
+if(num>=18){
+    alert("yes you can vote")
+}
+else  (num<18){
+    alert("you can't vote")
+}
+
