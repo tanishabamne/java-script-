2dd8c60e634fd803e9ba390e5cfb9448e1c2b057
tanishabamne <tanisha.bamne1980@gmail.com>
2024-11-19 09:57:26 -0800

    updated

---
+++ day4.js
@@ -0,0 +1,23 @@
+function go(){
+    let num1=confirm(" have u comlete course?");
+    let num2=confirm(" have u done certification");
+    if( num1 && num2)
+    {
+        alert(" congratulations! completed" )
+    }
+    else{
+        alert(" certification not completed.")
+    }
+    
+}
+// function easy(){
+//     let num= prompt("enter any number.")
+//     if(num%2 ==0){
+//         alert("number is even");
+
+//     }
+//     else{
+//         alert("number is odd");
+//     }
+// }
+funct
