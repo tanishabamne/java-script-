fabd4e26b3d2cb349c5d13ac1ac6b0f34bb4646b
tanishabamne <tanisha.bamne1980@gmail.com>
2024-11-20 09:29:56 -0800

    updated

--- day4.js
+++ day4.js
@@ -1,3 +1,13 @@
+    // que.1 program
+function q1(){
+    let age =prompt("Enter your age")
+    let result = age>=18 ? "Adult" : "Minor";
+    alert(result)
+
+    
+}
+                // ********************************que.2 program**********************************
+
 function go(){
     let num1=confirm(" have u comlete course?");
     let num2=confirm(" have u done certification");
@@ -10,14 +20,47 @@ function go(){
     }
     
 }
-// function easy(){
-//     let num= prompt("enter any number.")
-//     if(num%2 ==0){
-//         alert("number is even");
+                      //*******************************que03******************************************/
 
-//     }
-//     else{
-//         alert("number is odd");
-//     }
-// }
-funct
+function q3(){
+    let price =prompt("Enter price");
+    let coupen=confirm("Do you have discount coupen")
+    if(coupen ){
+    let coupen= price - (price*0.10)
+    alert(coupen)
+
+    }
+    else {
+        alert ("no discount")
+    }
+}
+
+           //*****************************************Que.04********************************************
+    
+ function q4(){
+    let num= prompt("enter your exam score")
+    if(num>=33){
+        alert("u r pass")
+    }
+    else{
+        alert("u r fail")
+    }
+
+ }
+
+
+
+            //   que.5 
+function easy(){
+    let num= prompt("enter any number.")
+    if(num%2 ==0){
+        alert("number is even");
+
+    }
+    else{
+        alert("number is odd");
+    }
+}
+
+
+                        //
